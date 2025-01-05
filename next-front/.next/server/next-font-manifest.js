43fa7b36ed71ed42c406303a660fcb85de806bb8
MasaG6014 <g2002masa@gmail.com>
2025-01-05 21:50:34 +0900

    deleted .next

--- next-front/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/masanari/sakoLab/卒論関係/code/my_test/my_vote/next-front/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
